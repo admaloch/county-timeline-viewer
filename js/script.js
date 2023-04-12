771f82a48a9a6ac1b3638b7164943d5b08eb7e37
Andrew Maloch <admaloch91@gmail.com>
2023-04-12 13:00:32 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -113,17 +113,19 @@ yearInput.addEventListener('keyup', function (e) {
     setTimeout(() => {
         genYearIndex()
         addCountyPeriodItems()
-        if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
-            var viewportmeta = document.querySelector('meta[name="viewport"]');
-            if (viewportmeta) {
-                viewportmeta.setAttribute('content', 'width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0');
-                viewportmeta.setAttribute('content', 'width=device-width, minimum-scale=1.0, initial-scale=1.0');
-            }
-        }
+        
     }, timeOutNum)
 });
 
-
+yearInput.addEventListener('blur', function (e) {
+    if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+        var viewportmeta = document.querySelector('meta[name="viewport"]');
+        if (viewportmeta) {
+            viewportmeta.setAttribute('content', 'width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0');
+            viewportmeta.setAttribute('content', 'width=device-width, minimum-scale=1.0, initial-scale=1.0');
+        }
+    }
+})
 
 // reset button under narrow by year form - reset timeline and map to default
 document.querySelector('#reset-results-btn').addEventListener('click', () => {
